5c049db2c675926ece9eab6af6e55002aef858db
Rajesh Kumar <95562281+rajeshcse2123@users.noreply.github.com>
2023-05-14 20:06:49 +0530

    Add files via upload

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,1 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { useReducer } from '../feature/userSlice';
 
+export default configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
